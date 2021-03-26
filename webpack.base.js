698c46ebbd7c721e764a39ffe189f1737d61e061
Marcin <zolkiewski.marcin@gmail.com>
2021-03-26 13:48:24 +0100

    separate configs for environments

---
+++ webpack.base.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = (mode) => ({
+  target: 'webworker',
+  output: {
+    filename: `worker.${mode}.js`,
+    path: path.join(__dirname, 'dist'),
+  },
+  mode,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      INFURA_API_KEY: undefined,
+      INFURA_NETWORK: undefined,
+      AUTOMATE_PAYMENT_KEY: undefined,
+      AUTOMATE_API_URL: undefined,
+    }),
+  ],
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,10 @@
+const path = require('path')
 const { config } = require('dotenv')
 const createConfig = require('./webpack.base')
 
-config()
+config({
+  path: path.join(__dirname, '.env.production'),
+})
 
 const mode = process.env.NODE_ENV || 'production'
 
