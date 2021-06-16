837554529e970818e3a1d29059b22078e3e55cb3
Marcin <zolkiewski.marcin@gmail.com>
2021-06-16 15:58:31 +0200

    fixed cache

--- src/cache.ts
+++ src/cache.ts
@@ -1,9 +1,12 @@
+// md5 to ensure key length does not go above max 512 chars allowed
+import md5 from 'md5'
+
 async function put(key: string, value: string): Promise<void> {
-  await AUTOMATE_PROXY_KV.put(key, value, { expirationTtl: 600 })
+  await AUTOMATE_PROXY_KV.put(md5(key), value, { expirationTtl: 600 })
 }
 
 async function get(key: string): Promise<any> {
-  return AUTOMATE_PROXY_KV.get(key)
+  return AUTOMATE_PROXY_KV.get(md5(key))
 }
 
 export default {
