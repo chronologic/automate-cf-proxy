05c4d23206401e4956c93521d7ffc1634c29baa4
Marcin <zolkiewski.marcin@gmail.com>
2022-04-08 11:21:05 +0200

    added comment

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const AUTOMATE_MD5_ADDRESS = '0x00000000e7fdc80c0728d856260f92fde10af019'
+export const AUTOMATE_MD5_ADDRESS = '0x00000000e7fdc80c0728d856260f92fde10af019' // md5 hash of the word 'automate'
 
 export const XFAI_CONTRACT_ADDRESS = '0xd622dbd384d8c682f1dfe2ec18809c6bcd09bd40'
 export const MAX_XFAI_GAS_LIMIT = 510_000
