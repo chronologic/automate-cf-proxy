bdcedc0c34ce59c42a96fc48c6c4889e20465942
Marcin <zolkiewski.marcin@gmail.com>
2022-05-06 09:49:55 +0200

    support block checking by metamask when confirming tx

--- src/handlers/arbitrum.ts
+++ src/handlers/arbitrum.ts
@@ -25,21 +25,13 @@ async function fallbackHandler(parsedReq: IParsedRequest): Promise<IJsonRpcRespo
     method: 'POST',
   })
 
-  console.log(1111)
-
   try {
     const resBody = await proxyRes.json()
-
-    console.log(2222)
-
     // console.log('RES <---', resBody)
 
     return resBody
   } catch (e) {
-    console.log(3333)
     const resText = await proxyRes.text()
-
-    console.log(4444)
     console.error('ERROR!', resText)
 
     throw e
--- src/handlers/common.ts
+++ src/handlers/common.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers'
 import queryString from 'query-string'
 
 import cache from '../cache'
-import { AUTOMATE_MD5_ADDRESS, CHAIN_IDS } from '../constants'
+import { AUTOMATE_MD5_ADDRESS, AUTOMATE_MD5_BLOCKHASH, CHAIN_IDS, ZERO_ADDRESS } from '../constants'
 import { AUTOMATE_PAYMENT_KEY } from '../env'
 import {
   IAutomateMaxNonceResponse,
@@ -22,6 +22,7 @@ export function makeHandlers(fallbackHandler: InternalHandler): IInternalHandler
     eth_sendRawTransaction: handleSendRawTransaction,
     eth_getTransactionCount: makeHandleGetTransactionCount(fallbackHandler),
     eth_getTransactionReceipt: makeHandleGetTransactionReceipt(fallbackHandler),
+    eth_getBlockByHash: makeHandleGetBlockByHash(fallbackHandler),
     eth_call: makeHandleCall(fallbackHandler),
     cache_test: handleCacheTest,
   }
@@ -112,7 +113,7 @@ function makeHandleGetTransactionReceipt(fallbackHandler: InternalHandler): Inte
     }
 
     const res = {
-      blockHash: '0xe05c4628a167c11e6e2d9ac5e3366c2cb350234d56c669c6634b55f7e4458b36',
+      blockHash: AUTOMATE_MD5_BLOCKHASH,
       blockNumber: '0x1',
       contractAddress: null,
       cumulativeGasUsed: '0x1',
@@ -135,9 +136,57 @@ function makeHandleGetTransactionReceipt(fallbackHandler: InternalHandler): Inte
   }
 }
 
+function makeHandleGetBlockByHash(fallbackHandler: InternalHandler): InternalHandler {
+  return async (parsedReq: IParsedRequest) => {
+    const { params = [] } = parsedReq.body
+    const [hash] = params
+    const isAutomateBlock = hash === AUTOMATE_MD5_BLOCKHASH
+
+    if (isAutomateBlock) {
+      const timestamp = ethers.BigNumber.from(Math.floor(new Date().getTime() / 1000)).toHexString()
+      const res = {
+        number: '0xac8a4d',
+        hash: AUTOMATE_MD5_BLOCKHASH,
+        parentHash: '0x0afebc49287252fdcb52f8008291a7ffc18fd1c68a90d2121a60bf2fc9792b19',
+        mixHash: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        nonce: '0x0000000000000000',
+        sha3Uncles: '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
+        logsBloom:
+          '0x00800000000000000000000080000000000000000000000210000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000008000010000000000000000000000000000000001000000000000000010000000000000000000000000000000000000000000000000000000000000000000100000000000000040000040480000200000080020001000000000000000000100000000000000000000000000000000000040000000000001000002000000000000000000000000000000001001000000000000000000000000000000020000000000000000000000000000800000000080000000',
+        transactionsRoot: '0xc4e7dfe36d2701ac34bbf46dbc010fc0e688f1be818a71a9c6ad9aee992b4ebc',
+        stateRoot: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        receiptsRoot: '0x812a56370de84a46af95f87fe3b395fb30a583363b38ad2064a550b92ab20ddb',
+        miner: '0x0000000000000000000000000000000000000000',
+        difficulty: '0x0',
+        totalDifficulty: '0x0',
+        extraData: '0x',
+        size: '0x0',
+        gasLimit: '0xbc1100a',
+        gasUsed: '0x14106',
+        timestamp,
+        transactions: ['0x9b500769a66cc57e4df6be02e54017f4daf83ddc42c230db1e194732cb6d8c60'],
+        uncles: [],
+        l1BlockNumber: '0xe0a4e8',
+      }
+
+      return {
+        id: parsedReq.body.id,
+        jsonrpc: parsedReq.body.jsonrpc,
+        result: res,
+      }
+    }
+
+    return fallbackHandler(parsedReq)
+  }
+}
+
 function makeHandleCall(fallbackHandler: InternalHandler): InternalHandler {
   return async (parsedReq: IParsedRequest) => {
-    if (parsedReq.body.params && parsedReq.body.params[0] && parsedReq.body.params[0].to === AUTOMATE_MD5_ADDRESS) {
+    const { params = [] } = parsedReq.body
+    const [from, to] = params
+    const isAutomateCheck = from === ZERO_ADDRESS && to === AUTOMATE_MD5_ADDRESS
+
+    if (isAutomateCheck) {
       const queryParams = { ...parsedReq.queryParams }
       delete (queryParams as any).apiKey
 
